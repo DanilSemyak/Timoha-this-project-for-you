08d067e5349443e00a452fd2d89c573b43a4b67c
Danil Semenchin <semen4in.d@gmail.com>
2023-12-20 20:57:19 +0300

    Выгрузил код

---
+++ Code_S#_graphics.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(773, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "go";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(624, 472);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "10";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 475);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 530);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Code_S#_graphics.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Random rnd = new Random();
+
+
+
+            g.Clear(Color.White);
+            int kolPoint = int.Parse(textBox1.Text);
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < kolPoint; i++)
+            {
+                List<int> arr1 = new List<int>();
+                int x1 = rnd.Next(10, 600);
+                int y1 = rnd.Next(10, 350);
+                g.DrawRectangle(Pens.Black, x1, y1, 1, 1);
+                arr1.Add(x1);
+                arr1.Add(y1);
+                arr.Add(arr1);
+
+            }
+
+            double s = int.MaxValue;
+            List<Point> arr3 = new List<Point> {new Point(0,0), new Point(2, 0), new Point(1, 0)};
+            for (int i = 0; i < kolPoint - 2; i++)
+            {
+                for (int j = i + 1; j < kolPoint - 1; j++)
+                {
+
+                    Point point1 = new Point(arr[i][0], arr[i][1]);
+                    Point point2 = new Point(arr[j][0], arr[j][1]);
+                    //g.DrawLine(Pens.Black, point1, point2);
+                    double k = (arr[j][1] - arr[i][1]) / (arr[j][0] - arr[i][0]);
+                    double b = arr[i][1] - (k * arr[i][0]);
+                    for (int f = j + 1; f < kolPoint; f++)
+                    {
+                        if (((k * arr[f][0]) + b) != arr[f][1])
+                        {
+                            double ss = 0.5 * Math.Abs(((arr[j][1] - arr[i][1]) * (arr[f][0] - arr[i][0]) - (arr[f][1] - arr[i][1]) * (arr[j][0] - arr[i][0])));
+                            if (ss < s)
+                            {
+                                s = ss;
+
+                                Point p1 = new Point(arr[i][0], arr[i][1]);
+                                Point p2 = new Point(arr[j][0], arr[j][1]);
+                                Point p3 = new Point(arr[f][0], arr[f][1]);
+
+                                arr3[0] = p1;
+                                arr3[1] = p2;
+                                arr3[2] = p3;
+                            }
+                        }
+                    }
+                }
+            }
+            g.DrawLine(Pens.Black, arr3[0], arr3[1]);
+            g.DrawLine(Pens.Black, arr3[1], arr3[2]);
+            g.DrawLine(Pens.Black, arr3[2], arr3[0]);
+            label1.Text = "Площадь треушолника: " + s;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
